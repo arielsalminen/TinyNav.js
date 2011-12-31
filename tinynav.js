ed1e4c147cd3019d2e510f06611bb47b694c0261
Viljami S <viljami.salminen@gmail.com>
2011-12-31 18:05:49 +0200

    v1.0

--- tinynav.js
+++ tinynav.js
@@ -1,45 +1,31 @@
 /*! TinyNav.js v1.0. (c) 2011 Viljami Salminen. MIT License. github.com/viljami/TinyNav.js */
 (function ($) {
   $.fn.tinyNav = function (options) {
-
-    // Settings
     var settings = {
-      'activeClass' : 'active'
+      'active' : 'selected'
     };
-
     return this.each(function () {
       var $this = $(this),
-        $sel = $('<select class="tinynav"></select>');
-
+        $select = $('<select class="tinynav"></select>');
       if (options) {
         $.extend(settings, options);
       }
-
-      // Create select if $this is a list
       if ($this.is('ul,ol')) {
-
         o = '';
-
         $this.find('a').each(function () {
           o +=
             '<option value="' + $(this).attr('href') + '">' +
             $(this).text() +
             '</option>';
         });
-
-        $sel
-          .append(o)
-          .find(':eq(' + $('li').index($('li.' + settings.activeClass)) + ')')
+        $select.append(o)
+          .find(':eq(' + $('li').index($('li.' + settings.active)) + ')')
           .attr('selected', 'selected');
-
-        $sel.bind('change', function () {
+        $select.bind('change', function () {
           document.location.href = $(this).val();
         });
-
-        $this.after($sel);
-
+        $this.after($select);
       }
-
     });
   };
 })(jQuery);
