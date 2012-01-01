4bd269dda8aadf1a62b06894881d90c4ba183aa1
Viljami S <viljami.salminen@gmail.com>
2012-01-01 11:33:35 +0200

    Fixes the namespace

--- tinynav.js
+++ tinynav.js
@@ -1,30 +1,37 @@
 /*! TinyNav.js v1.0. (c) 2011 Viljami Salminen. MIT License. github.com/viljami/TinyNav.js */
 (function ($) {
+  var inst = 0;
   $.fn.tinyNav = function (options) {
     var settings = {
-      'active' : 'selected'
+      'active' : 'selected',
+      'namespace' : 'tinynav'
     };
     return this.each(function () {
+      inst = inst + 1;
       var $this = $(this),
-        $select = $('<select class="tinynav"></select>');
+        namespace = settings.namespace,
+        $select = $('<select class="' + namespace + ' ' + namespace + inst + '"></select>');
       if (options) {
         $.extend(settings, options);
       }
       if ($this.is('ul,ol')) {
+        $this.addClass('l_' + namespace + inst);
         o = '';
-        $this.find('a').each(function () {
+        $('.l_' + namespace + inst).find('a').each(function () {
           o +=
             '<option value="' + $(this).attr('href') + '">' +
             $(this).text() +
             '</option>';
         });
-        $select.append(o)
-          .find(':eq(' + $('li').index($('li.' + settings.active)) + ')')
+        $select
+          .append(o)
+          .find(':eq(' + $('.l_' + namespace + inst + ' li')
+          .index($('.l_' + namespace + inst + ' li.' + settings.active)) + ')')
           .attr('selected', 'selected');
         $select.bind('change', function () {
           document.location.href = $(this).val();
         });
-        $this.after($select);
+        $('.l_' + namespace + inst).after($select);
       }
     });
   };
