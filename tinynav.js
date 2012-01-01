9581e7cfeb81d118ccb2ae9e5cc210462dccef6c
Viljami S <viljami.salminen@gmail.com>
2012-01-01 14:05:30 +0200

    Some optimization

--- tinynav.js
+++ tinynav.js
@@ -1,23 +1,23 @@
-/*! TinyNav.js v1.0. (c) 2011 Viljami Salminen. MIT License. github.com/viljami/TinyNav.js */
+/*! TinyNav.js v1.0. (c) 2011-2012 Viljami Salminen. MIT License. github.com/viljami/TinyNav.js */
 (function ($) {
-  var inst = 0;
+  var i = 0;
   $.fn.tinyNav = function (options) {
     var settings = {
       'active' : 'selected',
       'namespace' : 'tinynav'
     };
     return this.each(function () {
-      inst = inst + 1;
+      i = i + 1;
       var $this = $(this),
         namespace = settings.namespace,
-        $select = $('<select class="' + namespace + ' ' + namespace + inst + '"></select>');
+        $select = $('<select class="' + namespace + ' ' + namespace + i + '"></select>');
       if (options) {
         $.extend(settings, options);
       }
       if ($this.is('ul,ol')) {
-        $this.addClass('l_' + namespace + inst);
+        $this.addClass('l_' + namespace + i);
         o = '';
-        $('.l_' + namespace + inst).find('a').each(function () {
+        $('.l_' + namespace + i + ' a').each(function () {
           o +=
             '<option value="' + $(this).attr('href') + '">' +
             $(this).text() +
@@ -25,13 +25,13 @@
         });
         $select
           .append(o)
-          .find(':eq(' + $('.l_' + namespace + inst + ' li')
-          .index($('.l_' + namespace + inst + ' li.' + settings.active)) + ')')
+          .find(':eq(' + $('.l_' + namespace + i + ' li')
+          .index($('.l_' + namespace + i + ' li.' + settings.active)) + ')')
           .attr('selected', 'selected');
         $select.bind('change', function () {
           document.location.href = $(this).val();
         });
-        $('.l_' + namespace + inst).after($select);
+        $('.l_' + namespace + i).after($select);
       }
     });
   };
