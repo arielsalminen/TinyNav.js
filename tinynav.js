320bee49a076b716108ff5fe8ee6e91eedc2cc08
Krister Kari <krister.kari@webtehdas.fi>
2012-02-08 21:24:52 +0200

    Make it smaller

--- tinynav.js
+++ tinynav.js
@@ -1,11 +1,10 @@
 /*! TinyNav.js v1.01. (c)2011-2012 Viljami Salminen. MIT License. http://tinynav.viljamis.com */
-(function ($, window, i) {
+(function ($, window) {
   $.fn.tinyNav = function (options) {
     var settings = {
       'active' : 'selected'
     };
-    return this.each(function () {
-      i++;
+    return this.each(function (i) {
       var $nav = $(this),
         namespace = 'tinynav',
         namespace_i = namespace + i,
@@ -25,14 +24,14 @@
             );
           });
         $select
+          .change(function () {
+            window.location.href = $(this).val();
+          })
           .find(':eq(' + $(l_namespace_i + ' li')
           .index($(l_namespace_i + ' li.' + settings.active)) + ')')
           .attr('selected', true);
-        $select.change(function () {
-          window.location.href = $(this).val();
-        });
         $(l_namespace_i).after($select);
       }
     });
   };
-})(jQuery, this, 0);
+})(jQuery, this);
