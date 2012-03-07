6ee5237df47aa80f83898478eaa1cee245bd9516
Viljami S <viljami.salminen@gmail.com>
2012-03-07 19:59:35 +0200

    Some performance improvements and a bug fix

--- tinynav.js
+++ tinynav.js
@@ -1,38 +1,49 @@
-/*! TinyNav.js v1.01. (c)2011-2012 Viljami Salminen. MIT License. http://tinynav.viljamis.com */
+/*! TinyNav.js v1.02. (c)2011-2012 Viljami Salminen. MIT License. http://tinynav.viljamis.com */
 (function ($, window, i) {
   $.fn.tinyNav = function (options) {
-    var settings = {
+
+    var settings = $.extend({
       'active' : 'selected'
-    };
+    }, options);
+
     return this.each(function () {
       i++;
+
       var $nav = $(this),
         namespace = 'tinynav',
         namespace_i = namespace + i,
         l_namespace_i = '.l_' + namespace_i,
-        $select = $('<select/>').addClass(namespace, namespace_i);
-      if (options) {
-        $.extend(settings, options);
-      }
+        $select = $('<select/>').addClass(namespace + ' ' + namespace_i);
+
       if ($nav.is('ul,ol')) {
+
+        var options = '';
+
         $nav
           .addClass('l_' + namespace_i)
-          .find('a').each(function () {
-            $select.append(
-              $('<option/>')
-               .text($(this).text())
-               .val($(this).attr('href'))
-            );
+          .find('a')
+          .each(function () {
+            options +=
+              '<option value="' + $(this).attr('href') + '">' +
+              $(this).text() +
+              '</option>';
           });
+
         $select
+          .append(options)
           .find(':eq(' + $(l_namespace_i + ' li')
           .index($(l_namespace_i + ' li.' + settings.active)) + ')')
           .attr('selected', true);
+
         $select.change(function () {
           window.location.href = $(this).val();
         });
+
         $(l_namespace_i).after($select);
+
       }
+
     });
+
   };
 })(jQuery, this, 0);
